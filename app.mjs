b76aecda686258a96143bd22c37feb9632e9c3e3
hashimzia <hz2776@nyu.edu>
2023-11-09 16:22:12 -0500

    first deploy attempt

--- app.mjs
+++ app.mjs
@@ -1,20 +1,20 @@
 import './config.mjs';
 import './db.mjs';
 import express from 'express';
-import { mongoose } from 'mongoose';
-import path from 'path';
-import { fileURLToPath } from 'url';
+// import { mongoose } from 'mongoose';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
 
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 app.get('/', (req, res) => {
     res.send("hi2");
 });
 
 app.get('/restaurants', (req, res) => {
-
+    res.send("restaurants page");
 });
 
 app.listen(process.env.PORT || 3000);
