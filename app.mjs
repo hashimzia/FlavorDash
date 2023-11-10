40a5b8273641c23e9e0d6a8b3b591e0656475cb8
hashimzia <hz2776@nyu.edu>
2023-11-09 20:59:22 -0500

    fake restaraunt test

--- app.mjs
+++ app.mjs
@@ -1,13 +1,18 @@
 import './config.mjs';
 import './db.mjs';
 import express from 'express';
-// import { mongoose } from 'mongoose';
+import mongoose from 'mongoose';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+
+
+// const User = mongoose.model("User");
+const Restaurant = mongoose.model('Restaurant');
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+app.use(express.urlencoded({ extended: false }));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -18,8 +23,35 @@ app.get('/', (req, res) => {
     res.render("home");
 });
 
+// app.get('/restaurants', (req, res) => {
+//     res.render("restaurants");
+// });
+
 app.get('/restaurants', (req, res) => {
-    res.render("restaurants");
+    const fakeRestaurent = {
+        name: "Hashim's Halal Cart",
+        food: [
+            {
+                name: "Chicken Over Rice",
+                price: 9.99,
+                ingredients: ["Love"],
+                description: "Filled w love"
+            }
+        ]
+    };
+
+    const sampleRestaurant = new Restaurant(fakeRestaurent);
+    sampleRestaurant.save()
+        .then(() => {
+            console.log('Restaurant saved successfully');
+            res.redirect('/');
+            return;
+        })
+        .catch(err => {
+            console.error('Error saving restaurant:', err);
+            res.status(500).send('Error saving restaurant');
+            return;
+        });
 });
 
 app.listen(process.env.PORT || 3000);
